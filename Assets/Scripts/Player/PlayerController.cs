024390882ad4108289d2ad06dd9db685f790e7a9
Phlares <ryon.everett@gmail.com>
2018-01-27 13:50:08 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    [Header("Player Movement Attributes")]
+    public PlayerMovementValues playerMovementValues;
+
+    private Vector3 mouseWorldPosition = Vector3.zero;
+
+    public void SetMousePosition(Vector3 newMousePosition)
+    {
+        mouseWorldPosition = newMousePosition;
+    }
+
+    public void SetNewMovePosition()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class PlayerMovementValues
+{
+    [Header("Player Movement Attributes")]
+    public float playerMoveToPositionSpeed = 10f;
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour 
+{
+    [Header("Player Input Attributess")]
+    public Vector3 playerMouseInput;
+
+    [Space(10)]
+    public float mouseHitPointVerticalAdjust = 1f;
+
+    [Space(10)]
+    public LayerMask backgroundMask;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown((0)))
+        {
+            GetPlayerMousePosition();
+        }
+    }
+
+    private void GetPlayerMousePosition()
+    {
+        playerMouseInput = ReturnPlayerMousePositionInWorld();
+    }
+
+    private Vector3 ReturnPlayerMousePositionInWorld()
+    {
+        Vector3 newPlayerMousePosition = Vector3.zero;
+
+        Ray newMousePointRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit newMouseRayHit;
+
+        if (Physics.Raycast(newMousePointRay, out newMouseRayHit, backgroundMask))
+        {
+            newPlayerMousePosition = newMouseRayHit.point;
+            newPlayerMousePosition.Set(newPlayerMousePosition.x, mouseHitPointVerticalAdjust, newPlayerMousePosition.z);
+        }
+
+        return newPlayerMousePosition;
+    }
+}
