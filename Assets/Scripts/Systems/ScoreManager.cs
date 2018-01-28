66b611e3bc4d7e1630038d5795b6960ac853514f
Phlares <ryon.everett@gmail.com>
2018-01-27 23:29:36 -0500

    Added Font and Score
    updated scene

---
+++ Assets/Scripts/Systems/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    [Header("We Aint Got Shit")]
+    public float playerScore;
+    public Text playerScoreText;
+
+	// Use this for initialization
+	void Start () {
+        playerScore = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MakeScoreHappen();
+
+    }
+
+    //Super secret Score Function
+    void MakeScoreHappen() {
+        playerScore += Time.time;
+        playerScoreText.text = playerScore.ToString();
+    }
+}
