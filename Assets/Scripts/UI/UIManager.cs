de700e3b447b824522773925d5a00edd8f027882
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2018-01-27 20:00:21 -0500

    Started working on Main Menu

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Container")]
+    public GameObject FadeIn;
+    public GameObject startButton;
+    public GameObject quitButton;
+
+
+
+
+}
