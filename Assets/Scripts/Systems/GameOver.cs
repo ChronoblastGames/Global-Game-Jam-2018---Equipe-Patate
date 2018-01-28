fae516ee25a01b102e4abff0bb4351e816c780a9
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2018-01-28 11:59:32 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Systems/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameoverUI;
+
+    public void YouDied()
+    {
+        gameoverUI.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
